9bd207329292d250e473ad8efee995faa42e39d8
gipsy <ter4444@gmail.com>
2023-05-04 19:23:33 +0300

    added middleware to save token to localStorage

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,6 +1,7 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import auth from '../features/auth/authSlice';
-import { api } from './services/api'
+import auth                                    from '../features/auth/authSlice';
+import { api }                from './services/api'
+import { listenerMiddleware } from "../middleware/auth";
 
 export const store = configureStore({
   reducer: {
@@ -9,6 +10,7 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware)
+    .prepend(listenerMiddleware.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
